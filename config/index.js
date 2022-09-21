427c01789a168429c7ece64c0ff4508f51e161e5
02xue <114041158+02xue@users.noreply.github.com>
2022-09-21 13:43:23 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wxd5b6a27e22c35cd8",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "0e298c53cb75b7efa5f1987cdabd3bd8",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -95,28 +95,38 @@ export const config = {
       ]
     },
     {
-      name: "老婆1",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
+      name: "啊，是小崔",
+      id: "oRnZg54KitVUL5gsuChMMLPBv2as",
+      useTemplateId: "jrvb-ZThP3HwCCnevjfqkvt2PVUl-mIIaX82TcbSwVg",
+      province: "陕西",
+      city: "宝鸡",
+      horoscopeDate: '06-01',
+      horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
+      festivals: [
+      {"type": "生日", "name": "小袁", "year": "2002", "date": "10-26"},
+      ],
+      customizedDateList: [
+      // 在一起的日子
+        {"keyword": "love_day", date: "2020-10-29"},
+      ]
     },
     {
-      name: "老婆2",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
+      name: "啊，是小袁",
+      id: "oRnZg57_cGssVNWDyQqWZt8wjQKY",
+      useTemplateId: "jrvb-ZThP3HwCCnevjfqkvt2PVUl-mIIaX82TcbSwVg",
+      province: "陕西",
+      city: "宝鸡",
+      horoscopeDate: '10-26',
+      horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
+      festivals: [
+      {"type": "生日", "name": "小崔", "year": "2002", "date": "06-01"},
+      ],
+      customizedDateList: [
+      // 在一起的日子
+        {"keyword": "love_day", date: "2020-10-29"},
+      ]
     },
     {
       name: "老婆3",
@@ -139,7 +149,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "A1QWHuiVahmhXVbc9GMz5EBWP8nqn3Lxb5XWI7C_GOA",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -147,7 +157,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oRnZg57_cGssVNWDyQqWZt8wjQKY",
     },
     // 你可以不断按格式往下增加
     // ...
